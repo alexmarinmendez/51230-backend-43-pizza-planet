c2a324f56e22140f526325f14867b7f54cc2f627
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-08-28 08:48:50 -0500

    first commit

---
+++ src/pizzas/schemas/pizza.schema.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type PizzaDocument = HydratedDocument<Pizza>;
+
+@Schema()
+export class Pizza {
+  @Prop()
+  name: string;
+  @Prop()
+  price: number;
+  @Prop()
+  size: string;
+}
+
+export const PizzaSchema = SchemaFactory.createForClass(Pizza);
